e885214f6b42970cc6e9d170a6d635d172f17e5e
tarasenkoevgenii <tjm-photo@mail.ru>
2018-03-20 07:25:44 +0300

    Inital commit: setup PHPUnit TestCase

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Class TestCase
+ * @package Tests
+ */
+class TestCase extends BaseTestCase
+{
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+/**
+ * Class ExampleTest
+ * @package Tests\Unitcomposer
+ */
+class ExampleTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
